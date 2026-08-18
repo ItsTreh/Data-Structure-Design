a0f9495d08ba08d3e35ee0524c8ba95335dde398
ItsTreh <173081031+ItsTreh@users.noreply.github.com>
2026-08-18 10:46:41 -0600

    Added arithmetic and sizoe of examples

--- Session1.c
+++ Session1.c
@@ -2,6 +2,8 @@
 
 int main(){
 
+//We have the basics of pointers here
+
 char c1 = 'A';
 char* pc1;
 
@@ -25,6 +27,25 @@ pc1 = &c2;
 *pc1 = 'D';
 
 printf("%c\n", c2);
+
+//We are getting to how to see the size of pointers
+
+printf("Size of char pointer: %lu\n", sizeof(char*));
+printf("Size of int pointer: %lu\n", sizeof(int*));
+printf("Size of float pointer: %lu\n", sizeof(float*));
+printf("Size of char: %lu\n", sizeof(char));
+printf("Size of int: %lu\n", sizeof(int));
+printf("Size of float: %lu\n", sizeof(float));
+
+//Now we can see arithmetic of pointers here
+
+int IntArray[5];
+
+printf("%p\n", IntArray);
+printf("%p\n", &IntArray);
+
+*IntArray = 5;
+printf("%d\n", IntArray[0]);
 
 return 0;
 }
